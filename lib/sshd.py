8848ba43e4389dc61c5028cb8c65b57a4de2c00a
Noah Zoschke <noah@heroku.com>
2012-06-30 17:13:37 -0700

    session command validation

--- lib/sshd.py
+++ lib/sshd.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
-# Copyright (c) Twisted Matrix Laboratories.
-# See LICENSE for details.
+# Copyright (c) Noah Zoschke
+# MIT license
 
 from twisted.cred import portal, checkers
 from twisted.cred.error import UnauthorizedLogin
@@ -12,12 +12,14 @@ from twisted.internet import reactor, protocol, defer
 from twisted.python import log, failure
 from zope.interface import implements
 import os
+import re
+import shlex
 import sys
 log.startLogging(sys.stderr)
 
 class RepoACL(SSHPublicKeyDatabase):
     ACL = {
-        "25:25:85:78:31:f7:6e:46:04:9a:08:9b:8a:11:5c:a7": ["code"]
+        "25:25:85:78:31:f7:6e:46:04:9a:08:9b:8a:11:5c:a7": ["code", "gentle-snow-22"]
     }
 
     def requestAvatarId(self, credentials):
@@ -41,12 +43,33 @@ class GitUser(avatar.ConchUser):
         self.channelLookup.update({ "session": session.SSHSession })
 
 class GitSession:    
-    def __init__(self, avatar):
-        pass
+    def __init__(self, user):
+        self.user = user
 
     def execCommand(self, proto, cmd):
-        print cmd
-        raise Exception("no executing commands")
+        # validate `git-upload-pack '/myrepo.git'` cmd
+        lexer = shlex.shlex(cmd)
+        lexer.whitespace_split = True
+        argv = [s for s in lexer]
+
+        if len(argv) != 2:
+            raise Exception("Invalid command")
+
+        if argv[0] not in ["git-upload-pack", "git-receive-pack"]:
+            raise Exception("Invalid command")
+            
+        m = re.match(r"'/([a-z0-9-]+).git'", argv[1])
+
+        # malformed or non shell safe path
+        if not m:
+            raise Exception("Invalid path")
+
+        # not in ACL for SSH fingerprint
+        if m.group(1) not in self.user.repos:
+            raise Exception("Invalid path")
+
+        # forward SSH connection
+        # self.process = reactor.spawnProcess( ... )
 
     def getPty(self, term, windowSize, attrs):
         raise Exception("no ptys")
