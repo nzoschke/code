596ef6302be0d0f19f1a364aecb8753f8a325c9b
Noah Zoschke <noah@heroku.com>
2012-07-05 09:17:49 -0700

    split off known_hosts from Compiler API

--- etc/session/ssh-forward.sh
+++ etc/session/ssh-forward.sh
@@ -19,9 +19,10 @@ log fn=setup code=$HTTP_CODE
 [[ $HTTP_CODE == 200 ]] || { echo "invalid path"; exit 1; }
 
 log fn=forward
-csplit -sf c compiler.conf "/^#---/"   # Compiler API returns host, port and private key config
-cat c00 >> ssh.conf                    # append hostname/port config
-cat c01 >> id_rsa                      # save ssh private key
+csplit -sf c compiler.conf "/^##/" "{1}"  # Compiler API returns host, port and SSH key config
+cat c00 >> ssh.conf                       # append hostname/port config
+cat c01 >> id_rsa                         # save user private key
+cat c02 >> known_hosts                    # save host public key
 chmod 600 id_rsa
 ssh -F ssh.conf -C "$@" | tee ssh.log
 log fn=forward code=${PIPESTATUS[0]}
