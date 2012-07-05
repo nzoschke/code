482b674782f0475054026beb832edbf2c8d43252
Noah Zoschke <noah@heroku.com>
2012-07-05 08:29:41 -0700

    unindent for heredocs

--- lib/code.rb
+++ lib/code.rb
@@ -14,3 +14,12 @@ module Code
     end
   end
 end
+
+class String
+  # Strip leading whitespace from each line that is the same as the 
+  # amount of whitespace on the first line of the string.
+  # Leaves _additional_ indentation on later lines intact.
+  def unindent
+    gsub /^#{self[/\A\s*/]}/, ''
+  end
+end
