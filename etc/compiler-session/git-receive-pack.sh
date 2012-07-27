f3b3af2cc46e36c27c68bb7e5f630fc9a500e071
Noah Zoschke <noah@heroku.com>
2012-07-26 23:11:39 -0700

    DRY compiler session init scripts

--- etc/compiler-session/git-receive-pack.sh
+++ etc/compiler-session/git-receive-pack.sh
@@ -1,34 +1,8 @@
 #!/bin/bash
 set -e
 
-function _exit() {
-  tmp=$(pwd)
-  [[ "$tmp" == */tmp* ]] && rm -rf $tmp             # only remove tmp-ish dirs
-}
-trap _exit EXIT
-
-function log() {
-  echo app=codon file=git-receive-pack.sh "$@" >&3  # FD 3 connected to parent STDERR
-}
-
-export GIT_DIR=$(pwd)/repo.git
-
 echo -ne "Heroku preparing repository..." >&2
-
-# TODO: replace with bin/s3 call!
-log fn=get_repo
-HTTP_CODE=$(curl -K curl_get_repo.conf)
-log fn=get_repo code=$HTTP_CODE
-
-(
-  git bundle verify file.bundle \
-    && git clone --bare file.bundle $GIT_DIR \
-    || git init  --bare $GIT_DIR
-) 1>&3 2>&3
-
+source init.sh
 echo " done" >&2
 
-mkdir -p            $GIT_DIR/hooks
-cp pre-receive.sh   $GIT_DIR/hooks/pre-receive
-cp post-receive.sh  $GIT_DIR/hooks/post-receive
-git-receive-pack    $GIT_DIR
+git-receive-pack $GIT_DIR
--- etc/compiler-session/init.sh
+++ etc/compiler-session/init.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
-set -ex
-
-function _exit() {
-  tmp=$(pwd)
-  #[[ "$tmp" == */tmp* ]] && rm -rf $tmp             # only remove tmp-ish dirs
-}
-trap _exit EXIT
+set -e
 
 function log() {
-  echo app=codon file=init.sh "$@"
+  echo app=codon file=init.sh pwd=$(pwd) "$@" >&3
 }
 
 export GIT_DIR=$(pwd)/repo.git
 
-# TODO: replace with bin/s3 call!
-log fn=get_repo
-HTTP_CODE=$(curl -K curl_get_repo.conf)
-log fn=get_repo code=$HTTP_CODE
+if [ ! -d "$GIT_DIR" ]; then
+  # TODO: replace with bin/s3 call!
+  log fn=get_repo
+  HTTP_CODE=$(curl -K curl_get_repo.conf)
+  log fn=get_repo code=$HTTP_CODE
 
-git bundle verify file.bundle \
-  && git clone --bare file.bundle $GIT_DIR \
-  || git init  --bare $GIT_DIR
+  (
+    git bundle verify file.bundle \
+      && git clone --bare file.bundle $GIT_DIR \
+      || git init  --bare $GIT_DIR
+  ) 1>&3 2>&3
 
-mkdir -p            $GIT_DIR/hooks
-cp pre-receive.sh   $GIT_DIR/hooks/pre-receive
-cp post-receive.sh  $GIT_DIR/hooks/post-receive
+  mkdir -p            $GIT_DIR/hooks
+  cp pre-receive.sh   $GIT_DIR/hooks/pre-receive
+  cp post-receive.sh  $GIT_DIR/hooks/post-receive
+fi
+
