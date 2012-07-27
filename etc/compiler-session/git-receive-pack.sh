010dc76fdc1c765eb1ad3445f20afb81587bcd3b
Noah Zoschke <noah@heroku.com>
2012-07-27 08:28:06 -0700

    dont init if GIT_DIR exists

--- etc/compiler-session/git-receive-pack.sh
+++ etc/compiler-session/git-receive-pack.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -e
 
-echo -ne "Heroku preparing repository..." >&2
-source init.sh
-echo " done" >&2
+export GIT_DIR=$(pwd)/repo.git
+
+if [ ! -d "$GIT_DIR" ]; then
+  echo -ne "Heroku preparing repository..." >&2
+  source init.sh
+  echo " done" >&2
+fi
 
 git-receive-pack $GIT_DIR
