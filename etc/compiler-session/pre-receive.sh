92319b0c496938f59a42b29bc1784315356a2d71
Noah Zoschke <noah@heroku.com>
2012-08-08 14:53:31 -0700

    anvil build proof of concept

--- etc/compiler-session/pre-receive.sh
+++ etc/compiler-session/pre-receive.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
-
 echo "Heroku receiving push..."
+
+GIT_DIR=$(pwd)
+WORK_TREE=$(dirname $GIT_DIR)/app # GIT_WORKING_TREE ?
+
+export HOME=/Users/noah
+
+read oldrev newrev ref
+if [ "$ref" = "refs/heads/anvil" ]; then
+  mkdir -p $WORK_TREE
+  git --work-tree=$WORK_TREE checkout -f $newrev 2>/dev/null
+  SLUG_URL=$(heroku build -p)
+  echo "Heroku releasing $SLUG_URL"
+  exit 0
+fi
