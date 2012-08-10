7c96540b29c2a98a1e6691b8e908cb409a5205af
Noah Zoschke <noah@heroku.com>
2012-08-10 14:37:36 -0700

    stow with anvil

--- etc/compiler-session/pre-receive.sh
+++ etc/compiler-session/pre-receive.sh
@@ -2,12 +2,12 @@
 echo "Heroku receiving push..."
 
 GIT_DIR=$(pwd)
-WORK_TREE=$(dirname $GIT_DIR)/app # GIT_WORKING_TREE ?
-
-export HOME=/Users/noah
+WORK_DIR=$(dirname $GIT_DIR)/app # GIT_WORKING_TREE ?
 
 read oldrev newrev ref
-mkdir -p $WORK_TREE
-git --work-tree=$WORK_TREE checkout -f $newrev 2>/dev/null
+
+mkdir -p $WORK_DIR
+git --work-tree=$WORK_DIR checkout -f $newrev 2>/dev/null
 cd /app/vendor/anvil
-bin/compile $WORK_TREE
+bin/compile $WORK_DIR
+bin/stow    $WORK_DIR
