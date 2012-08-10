10326bb74120f49d4d979e02bed863f53faf42ff
Noah Zoschke <noah@heroku.com>
2012-08-10 13:04:27 -0700

    use vendored anvil

--- etc/compiler-session/pre-receive.sh
+++ etc/compiler-session/pre-receive.sh
@@ -7,10 +7,7 @@ WORK_TREE=$(dirname $GIT_DIR)/app # GIT_WORKING_TREE ?
 export HOME=/Users/noah
 
 read oldrev newrev ref
-if [ "$ref" = "refs/heads/anvil" ]; then
-  mkdir -p $WORK_TREE
-  git --work-tree=$WORK_TREE checkout -f $newrev 2>/dev/null
-  SLUG_URL=$(heroku build -p)
-  echo "Heroku releasing $SLUG_URL"
-  exit 0
-fi
+mkdir -p $WORK_TREE
+git --work-tree=$WORK_TREE checkout -f $newrev 2>/dev/null
+cd /app/vendor/anvil
+bin/compile $WORK_TREE
